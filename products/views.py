bc505421b5b691da1b0d151958b3d81ad145520c
Vin <maharjanebik061@gmail.com>
2022-10-24 23:56:37 +0545

    search feature added

--- products/views.py
+++ products/views.py
@@ -6,5 +6,13 @@ from .models import Product
 
 def home(request):
     products = Product.objects.all()
-    return render(request, 'products/home.html', {'products': products})
+    if request.method == 'GET':
+        return render(request, 'products/home.html', {'products': products})
+    elif request.method == 'POST':
+        search_text = request.POST['search-data']
+        search_products = []
+        for product in products:
+            if search_text in product.name:
+                search_products.append(product)
+        return render(request, 'products/search.html', {'products': search_products})
 
